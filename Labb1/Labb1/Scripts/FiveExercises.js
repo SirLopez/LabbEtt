7d7998027b3d27459a6b60f8ea4712d560dbe842
SirLopez <daniel.lopez@live.se>
2015-12-21 10:50:38 +0100

    second commit

--- Labb1/Labb1/Scripts/FiveExercises.js
+++ Labb1/Labb1/Scripts/FiveExercises.js
@@ -1,4 +1,6 @@
-﻿var inp;
+﻿// Task 1
+
+var inp;
 
 var numberInput = document.getElementById("task1answer");
 numberInput.addEventListener('mouseover', displaySqrt, false);
@@ -14,8 +16,35 @@ function calculatesqrt() {
     }
 }
 
-
 function displaySqrt() {
     inp = calculatesqrt();
     document.getElementById("task1answer").innerHTML = inp;
+}
+
+
+// Task 2
+
+var com;
+
+var salesInput = document.getElementById("task2answer");
+salesInput.addEventListener('click', displaySalary, false);
+
+function calculateSalary() {
+    var totalSales = document.getElementById("task2input").value;
+
+    if (isNaN(totalSales)) {
+        return "Sorry, digits only!";
+    }
+    if (totalSales <= 0) {
+        return 15000;
+    }
+    else {
+        return ((9 / 100) * totalSales) + 15000;
+    }
+}
+
+function displaySalary() {
+    com = calculateSalary();
+    var commision = com - 15000;
+    document.getElementById("task2answer").innerHTML = Math.round(com * 100) / 100 + " SEK, where the commision is " + Math.round(commision * 100) / 100 + " SEK (9%).";
 }
