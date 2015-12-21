8d0768e21c09c34ebb92052d38381133f35ce480
SirLopez <daniel.lopez@live.se>
2015-12-21 10:47:51 +0100

    First commit.

---
+++ Labb1/Labb1/Scripts/FiveExercises.js
@@ -0,0 +1,21 @@
+﻿var inp;
+
+var numberInput = document.getElementById("task1answer");
+numberInput.addEventListener('mouseover', displaySqrt, false);
+
+function calculatesqrt() {
+    var number = document.getElementById("task1input").value;
+
+    if (isNaN(number)) {
+        return "Sorry, digits only!";
+    }
+    else {
+        return Math.sqrt(number);
+    }
+}
+
+
+function displaySqrt() {
+    inp = calculatesqrt();
+    document.getElementById("task1answer").innerHTML = inp;
+}
